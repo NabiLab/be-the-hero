4aa31165a04aba4fd54e9e299e75dc0ac4c17eff
BiaNunes <anabinl@outlook.com>
2020-03-26 22:48:46 -0300

    Mobile da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,37 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-/** 
- * Rota / Recurso
- */
-
- /**
-  * Métodos HTTP
-  * 
-  * GET: Buscar uma informação do back-end
-  * POST: Criar uma informação no back-end
-  * PUT: Alterar uma informação no back-end
-  * DELETE: Deletar uma informação no back-end
-  */
-
-/**
- * Tipos de parâmetros:
- * 
- * Query Params: Parãmetros nomeados enviados na rota após o "?" (Filtros, paginação)
- * Rout Params: Parâmetros usados para identificar recursos
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
- /**
-  * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
-  * NoSQL: MongoDb, CouchDb, etc
-  */
-
-  /**
-   * Driver: SELECT * FROM users
-   * Query Builder: table('users').select('*').where()
-   */
-
-
-
 app.listen(3333);
---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes.js';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
+
+//192x192 1242x2436
